a10503a2010dff253aaef69df39bb126e09f9182
Mohit Soni <developer_mohitsoni@outlook.com>
2025-04-03 15:24:43 +0530

    refactor: remove cluster name from MongoDB connection URI for cleaner configuration

--- data/database.js
+++ data/database.js
@@ -4,9 +4,8 @@ const clusterAddress = process.env.MONGODB_CLUSTER_ADDRESS;
 const dbUser = process.env.MONGODB_USERNAME;
 const dbPassword = process.env.MONGODB_PASSWORD;
 const dbName = process.env.MONGODB_DB_NAME;
-const clusterName = process.env.MONGODB_CLUSTER_NAME;
 
-const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority&appName=devCluster`;
+const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri);
 
 console.log("Trying to connect to db");
